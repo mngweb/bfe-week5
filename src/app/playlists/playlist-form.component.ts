a6d0c9b03d488e73f2a9bec37a7cf1dcf66f49a9
mngweb <mangoweb@wp.pl>
2017-04-17 21:47:05 +0200

    Zadania 5.1-5.5

--- src/app/playlists/playlist-form.component.ts
+++ src/app/playlists/playlist-form.component.ts
@@ -3,48 +3,57 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'playlist-form',
   template: `
-        <div> 
+        <div>
+        
+         <!-- ZAD.3 -->
+         <!-- ZAD.5 -->
 
-        <!-- ZAD.3 -->
-
-        <form (submit)="save(playlist)">
+        <form (submit)="save(_playlistCopy)">
           <div class="form-group">
             <label>Nazwa:</label>
-            <input type="text" name="name" [(ngModel)]="playlist.name" class="form-control">
+            <input type="text" name="name" [(ngModel)]="_playlistCopy.name" class="form-control">
           </div>
           <div class="form-group">
             <label>Utwory:</label>
-            <input type="text" name="tracks" [value]="playlist.tracks + ' utwory'" disabled class="form-control">
+            <input type="text" name="tracks" [value]="_playlistCopy.tracks + ' utwory'" disabled class="form-control">
           </div>
           <div class="form-group">
             <label>Kolor:</label>
-            <input type="color" name="color" [(ngModel)]="playlist.color">
+            <input type="color" name="color" [(ngModel)]="_playlistCopy.color">
           </div>
           <div class="form-group">
-            <label><input type="checkbox" name="favourite" [(ngModel)]="playlist.favourite"> 
+            <label><input type="checkbox" name="favourite" [(ngModel)]="_playlistCopy.favourite"> 
             Ulubiona</label>
           </div>
           <div class="form-group">
             <button class="btn btn-success float-xs-right">Zapisz</button>
             <!-- <button class="btn btn-success float-xs-right" 
-                    (click)="save(playlist)">Zapisz</button> -->
+                    (click)="save(_playlistCopy)">Zapisz</button> -->
           </div>
         </form>
-
+        
         </div>
   `,
   styles: []
 })
 export class PlaylistFormComponent implements OnInit {
 
+  // ZAD.5
+  _playlistCopy = {};
+
   @Input()
-  playlist;
+  set playlist(playlist) {
+     this._playlistCopy = Object.assign({},playlist);
+  }
+
 
   @Output('saved')
   onSave = new EventEmitter();
 
+
+  // ZAD.5
   save(playlist){
-    this.onSave.emit(playlist)
+    this.onSave.emit(this._playlistCopy)
   }
 
   constructor() { }
